8c5e416a2732c79f5b2c11da4d780dd7102e7367
Brandon <brandoncyu@gmail.com>
2018-08-06 10:44:49 -0700

    changed some forms

--- scripts_org/templates/registerTemplate.js
+++ scripts_org/templates/registerTemplate.js
@@ -18,6 +18,14 @@ const registerTemplate = () => {
       <label for="register-ein">Your Organization's EIN Number</label><br>
       <input class="form-control" type="number" id="register-ein" placeholder="Please enter your organiation's EIN number" required="required">
     </div>
+    <div class="form-group">
+      <label for="register-url">A Link to Your Organization's Logo</label><br>
+      <input class="form-control" type="url" id="register-logo" placeholder="Please enter a photo link to your organization's logo" required="required">
+    </div>
+    <div class="form-group">
+      <label for="event-description">Your Organiation's Description</label>
+      <textarea id="event-description" name="register-description" type="text" class="form-control" rows="3" maxlength='255' placeholder="Enter your organization's description" required="required"></textarea>
+    </div>
     <button class="btn btn-success" type="submit">Submit</button>
   </form>`
 }
